5d13ef2f89fe541d8204d0033ebbb468fa67fdc2
0855164264max <max0855164264@gmail.com>
2024-03-09 17:03:04 +0700

    updateDB

--- dbconn.ts
+++ dbconn.ts
@@ -1,9 +1,15 @@
 import mysql from "mysql";
 
 export const conn = mysql.createPool({
+  // connectionLimit: 10,
+  // host: "localhost",
+  // user: "demo",
+  // password: "abc123",
+  // database: "work5",
+
   connectionLimit: 10,
-  host: "localhost",
-  user: "demo",
-  password: "abc123",
-  database: "work5",
+  host: "sql6.freemysqlhosting.net",
+  user: "sql6689854",
+  password: "RVwcFhvWiV",
+  database: "sql6689854",
 });
